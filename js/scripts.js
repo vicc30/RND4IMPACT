3f068b94ded5f7e94196a725302df24b2e69a6fc
Victor Cruz <vicc23@gmail.com>
2020-07-30 14:05:04 -0500

    Added bar graph and activities page content

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,50 @@ $(document).ready(function(){
         }
     });
 });
+
+var ctx = document.getElementById('myChart').getContext("2d");
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Millions Americans can benefit', 'Millions Americans depend on food banks', 'Millions Americans can be feeded with donations'],
+        datasets: [{
+            data: [74,37,22],
+            backgroundColor: [
+                'rgba(32, 40, 157, 0.75)',
+                'rgba(155, 32, 101, 0.75)',
+                'rgba(32, 157, 45, 0.75)',
+            ],
+            borderColor: [
+                'rgba(32, 40, 157, 1)',
+                'rgba(155, 32, 101, 1)',
+                'rgba(32, 157, 45, 1)'
+            ],
+            borderWidth: 1
+        }]
+        },
+    options:{
+        scales: {
+            xAxes: [{
+                barPercentage: 1.0
+            }],
+            yAxes: [{
+                type: "linear",
+                display: true,
+                ticks: {
+                    beginAtZero: true
+                },
+                // space between top and graph
+                offset: true
+            }],
+
+        },
+        //Remove legend
+        legend: {
+           display: false
+        },
+        //disable tooltip
+        tooltips: {
+            enabled: false
+        }
+    }
+});
